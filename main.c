f896d050267e4992fad326b856f2df98a475f6cb
zelfeim <zelfeim@mailbox.org>
2024-12-14 08:58:24 +0100

    basic algorithm structure

--- main.c
+++ main.c
@@ -1,3 +1,15 @@
+#include <bool.h>
+
+struct tile {
+    int x;
+    int y;
+    bool is_blocker;
+}
+
 int main() {
+    // read file to tile array
+
+    // algorithm returning shortest path
+
     return 0;
 }
